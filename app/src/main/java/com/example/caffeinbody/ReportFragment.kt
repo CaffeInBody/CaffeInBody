07b8cf1534ef527fb0497e5f9311ad51f9e34145
yeonank <yeonank99@gmail.com>
2022-04-10 13:26:09 +0900

    report layout 구성

--- app/src/main/java/com/example/caffeinbody/ReportFragment.kt
+++ app/src/main/java/com/example/caffeinbody/ReportFragment.kt
@@ -1,32 +1,27 @@
 package com.example.caffeinbody
 
+import android.graphics.Color
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.caffeinbody.databinding.FragmentReportBinding
+import com.github.mikephil.charting.charts.BarChart
+import com.github.mikephil.charting.charts.LineChart
+import com.github.mikephil.charting.components.Description
+import com.github.mikephil.charting.components.Legend
+import com.github.mikephil.charting.components.XAxis
+import com.github.mikephil.charting.components.YAxis
+import com.github.mikephil.charting.data.*
+import com.github.mikephil.charting.interfaces.datasets.ILineDataSet
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [ReportFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class ReportFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+    private val lineChartData = ArrayList<Entry>()
+    private val binding: FragmentReportBinding by lazy {
+        FragmentReportBinding.inflate(
+            layoutInflater
+        )
     }
 
     override fun onCreateView(
@@ -34,10 +29,14 @@ class ReportFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_report, container, false)
+        lineChartData.add(Entry(3.toFloat(), 10.toFloat()));
+        lineChartData.add(Entry(5.toFloat(), 1.toFloat()));
+        lineChartData.add(Entry(7.toFloat(), 20.toFloat()));
+        setChartView(binding.root)
+        return binding.root
     }
 
-    companion object {
+    /*companion object {
         /**
          * Use this factory method to create a new instance of
          * this fragment using the provided parameters.
@@ -55,5 +54,164 @@ class ReportFragment : Fragment() {
                     putString(ARG_PARAM2, param2)
                 }
             }
+    }*/
+
+    private fun setChartView(view: View) {
+        var chartWeek = binding.barchart
+        var chartWeekLine = binding.linechart
+        setWeek(chartWeek)
+        setWeekLine(chartWeekLine)
+    }
+
+    private fun initBarDataSet(barDataSet: BarDataSet) {
+        //Changing the color of the bar
+        barDataSet.color = Color.parseColor("#304567")
+        //Setting the size of the form in the legend
+        barDataSet.formSize = 15f
+        //showing the value of the bar, default true if not set
+        barDataSet.setDrawValues(false)
+        //setting the text size of the value of the bar
+        barDataSet.valueTextSize = 12f
+    }
+
+    private fun setWeekLine(lineChart: LineChart) {
+        /*val entries = mutableListOf<Entry>()  //차트 데이터 셋에 담겨질 데이터
+
+        for (item in chartData) {
+            entries.add(Entry(item.lableData.replace(("[^\\d.]").toRegex(), "").toFloat(), item.lineData.toFloat()))
+        }
+
+        //LineDataSet 선언
+        val lineDataSet: LineDataSet
+        lineDataSet = LineDataSet(entries, "라인챠트 예시")
+        lineDataSet.color = Color.BLUE  //LineChart에서 Line Color 설정
+        lineDataSet.setCircleColor(Color.DKGRAY)  // LineChart에서 Line Circle Color 설정
+        lineDataSet.setCircleHoleColor(Color.DKGRAY) // LineChart에서 Line Hole Circle Color 설정
+
+        val dataSets = ArrayList<ILineDataSet>()
+        dataSets.add(lineDataSet) // add the data sets
+
+        // create a data object with the data sets
+        val data = LineData(dataSets)
+
+        // set data
+        lineChart.setData(data)
+        lineChart.setDescription(null); //차트에서 Description 설정 삭제
+        */
+
+        val chartData = LineData()
+        val set1 = LineDataSet(lineChartData, "첫번째")
+        chartData.addDataSet(set1)
+        lineChart.setData(chartData)
+        lineChart.invalidate()
+    }
+
+    private fun setWeek(barChart: BarChart) {
+        initBarChart(barChart)
+
+        barChart.setScaleEnabled(false) //Zoom In/Out
+
+        val valueList = ArrayList<Double>()
+        val entries: ArrayList<BarEntry> = ArrayList()
+        val title = "걸음 수"
+
+        //input data
+        for (i in 0..7) {
+            valueList.add(i * 100.1)
+        }
+
+        //fit the data into a bar
+        for (i in 0 until valueList.size) {
+            val barEntry = BarEntry(i.toFloat(), valueList[i].toFloat())//월화수목금토일
+            entries.add(barEntry)
+        }
+        val barDataSet = BarDataSet(entries, title)
+        val data = BarData(barDataSet)
+        barChart.data = data
+        barChart.invalidate()
+    }
+
+    /*private fun setWeekLine(lineChart: LineChart) {
+        initBarChart(lineChart)
+
+        lineChart.setScaleEnabled(false) //Zoom In/Out
+
+        val valueList = ArrayList<Double>()
+        val entries: ArrayList<BarEntry> = ArrayList()
+        val title = "걸음 수"
+
+        //input data
+        for (i in 0..5) {
+            valueList.add(i * 100.1)
+        }
+
+        //fit the data into a bar
+        for (i in 0 until valueList.size) {
+            val barEntry = BarEntry(i.toFloat(), valueList[i].toFloat())
+            entries.add(barEntry)
+        }
+        val barDataSet = BarDataSet(entries, title)
+        val data = BarData(barDataSet)
+        lineChart.data = data
+        lineChart.invalidate()
+    }*/
+
+    private fun initBarChart(barChart: BarChart) {
+        //hiding the grey background of the chart, default false if not set
+        barChart.setDrawGridBackground(false)
+        //remove the bar shadow, default false if not set
+        barChart.setDrawBarShadow(false)
+        //remove border of the chart, default false if not set
+        barChart.setDrawBorders(false)
+
+        //remove the description label text located at the lower right corner
+        val description = Description()
+        description.setEnabled(false)
+        barChart.setDescription(description)
+
+        //X, Y 바의 애니메이션 효과
+        barChart.animateY(1000)
+        barChart.animateX(1000)
+
+
+        //바텀 좌표 값
+        val xAxis: XAxis = barChart.getXAxis()
+        //change the position of x-axis to the bottom
+        xAxis.position = XAxis.XAxisPosition.BOTTOM
+        //set the horizontal distance of the grid line
+        xAxis.granularity = 1f
+        xAxis.textColor = Color.RED
+        //hiding the x-axis line, default true if not set
+        xAxis.setDrawAxisLine(false)
+        //hiding the vertical grid lines, default true if not set
+        xAxis.setDrawGridLines(false)
+
+
+        //좌측 값 hiding the left y-axis line, default true if not set
+        val leftAxis: YAxis = barChart.getAxisLeft()
+        leftAxis.setDrawAxisLine(false)
+        leftAxis.textColor = Color.RED
+
+
+        //우측 값 hiding the right y-axis line, default true if not set
+        val rightAxis: YAxis = barChart.getAxisRight()
+        rightAxis.setDrawAxisLine(false)
+        rightAxis.textColor = Color.RED
+
+
+        //바차트의 타이틀
+        val legend: Legend = barChart.getLegend()
+        //setting the shape of the legend form to line, default square shape
+        legend.form = Legend.LegendForm.LINE
+        //setting the text size of the legend
+        legend.textSize = 11f
+        legend.textColor = Color.YELLOW
+        //setting the alignment of legend toward the chart
+        legend.verticalAlignment = Legend.LegendVerticalAlignment.TOP
+        legend.horizontalAlignment = Legend.LegendHorizontalAlignment.CENTER
+        //setting the stacking direction of legend
+        legend.orientation = Legend.LegendOrientation.HORIZONTAL
+        //setting the location of legend outside the chart, default false if not set
+        legend.setDrawInside(false)
     }
 }
