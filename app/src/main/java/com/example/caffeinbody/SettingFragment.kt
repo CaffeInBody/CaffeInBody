a3f59b601da83e8b83ff7bbe78743be85022416c
HSHyeon <hshyeon86@gmail.com>
2022-04-16 14:38:09 +0900

    [UPDATE]setting fragment

--- app/src/main/java/com/example/caffeinbody/SettingFragment.kt
+++ app/src/main/java/com/example/caffeinbody/SettingFragment.kt
@@ -34,7 +34,7 @@ class DecafFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_decaf, container, false)
+        return inflater.inflate(R.layout.fragment_setting, container, false)
     }
 
     companion object {
