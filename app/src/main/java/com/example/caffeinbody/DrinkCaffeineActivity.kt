432e9ae334ec9f3f301abf816bff483e4a8b7c4a
yeonank <yeonank99@gmail.com>
2022-05-02 17:41:54 +0900

    drink_caffeine 연결

--- app/src/main/java/com/example/caffeinbody/DrinkCaffeineActivity.kt
+++ app/src/main/java/com/example/caffeinbody/DrinkCaffeineActivity.kt
@@ -1,14 +1,52 @@
 package com.example.caffeinbody
 
+import android.content.Intent
+import android.graphics.Color
+import android.graphics.drawable.Drawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.example.caffeinbody.databinding.ActivityDrinkCaffeineBinding
+import com.example.caffeinbody.databinding.RegisterLayoutDrinkBinding
 
 class DrinkCaffeineActivity : AppCompatActivity() {
+    private val binding: ActivityDrinkCaffeineBinding by lazy{
+        ActivityDrinkCaffeineBinding.inflate(
+            layoutInflater
+        )
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_drink_caffeine)
+        setContentView(binding.root)
+        //setContentView(R.layout.activity_drink_caffeine)
         intent.getIntExtra("id",1) //data id 가져오기
 
+        val size1_btn = binding.size1
+        val size2_btn = binding.size2
+        val size3_btn = binding.size3
 
+        size1_btn.setOnClickListener {
+            size1_btn.setBackgroundResource(R.drawable.selected)
+            size2_btn.setBackgroundResource(R.drawable.btn_select)
+            size3_btn.setBackgroundResource(R.drawable.btn_select)
+        }
+
+        size2_btn.setOnClickListener {
+            size1_btn.setBackgroundResource(R.drawable.btn_select)
+            size2_btn.setBackgroundResource(R.drawable.selected)
+            size3_btn.setBackgroundResource(R.drawable.btn_select)
+        }
+
+        size3_btn.setOnClickListener {
+            size1_btn.setBackgroundResource(R.drawable.btn_select)
+            size2_btn.setBackgroundResource(R.drawable.btn_select)
+            size3_btn.setBackgroundResource(R.drawable.selected)
+        }
+
+
+
+        binding.save.setOnClickListener {
+            finish()
+        }
     }
 }
