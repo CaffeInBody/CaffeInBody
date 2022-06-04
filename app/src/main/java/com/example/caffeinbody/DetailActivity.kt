d910ef39eb79789437eccf33412a1cc39cf3cc68
yeonank <yeonank99@gmail.com>
2022-06-04 16:28:00 +0900

    반감기 그래프 수정

--- app/src/main/java/com/example/caffeinbody/DetailActivity.kt
+++ app/src/main/java/com/example/caffeinbody/DetailActivity.kt
@@ -20,7 +20,7 @@ import kotlin.math.pow
 //라인차트 그리기
 class DetailActivity : AppCompatActivity() {
     val basicTime = 5
-    var beforeTime = 0f
+
     private val lineChartData = ArrayList<Entry>()
     private val binding: ActivityDetailBinding by lazy {
         ActivityDetailBinding.inflate(
@@ -61,61 +61,61 @@ class DetailActivity : AppCompatActivity() {
                 caffeines.add(a2.optString(i).toFloat())
                 Log.e("time", "times: " + times[i]+ " Caffeines: " + caffeines[i] + " " + i + "번째")
             }//여기까지가 입력한 시간과 양 어레이로 가져오기
-            times.add(0f)
-            var caffeineRemain = caffeines[timeI]!!
-            currentTime = times[timeI] + (halfTime * count).toFloat()
-            lineChartData.add(Entry(currentTime, caffeineRemain))
-            while (caffeineRemain!! >= 10){
-                caffeineRemain = caffeines[timeI]!!/((2.0).pow(count)).toFloat()
-                currentTime = times[timeI] + (halfTime * count).toFloat()
-                //Log.e("times[i]", times[timeI + 1])
-                if (times[timeI + 1] != 0f){//더 추가된 카페인 데이터가 있으면
-                    Log.e("time", "야야야야야!" + timeI)
+            times.add(0f)//공백 넣어줌
 
-                    if (currentTime+ basicTime < times[timeI + 1]){//5시간 뒤보다 뒤에 추가됐으면
-                        Log.e("time", "여기는 1")
+            //첫번째 점 그려줌
+            var caffeineRemain = caffeines[timeI]!!
+            currentTime = times[timeI] + (halfTime * count++).toFloat()
+            lineChartData.add(Entry(currentTime, caffeineRemain))
+
+            while (caffeineRemain!! >= 10){
+                Log.e("time", "남은 카페인 : $caffeineRemain")
+                if (times[timeI + 1] != 0f){//더 추가된 카페인 데이터가 있으면
+                    if (currentTime+ basicTime < times[timeI + 1]){//5시간 뒤보다 후에 추가됐으면
+                        currentTime = times[timeI] + (halfTime * count).toFloat()
+                        caffeineRemain = caffeineRemain!!/(2.0).toFloat()
                         lineChartData.add(Entry(currentTime, caffeineRemain))
+                        Log.e("time", "여기는 1")
                     }else{//그래프 중간에 끼워야 할 때
                         Log.e("time", "여기는 2")
-                        //lineChartData.add(Entry(currentTime, caffeineRemain))
-                        val timeGap = times[timeI + 1] - currentTime
-                        caffeineRemain = caffeineRemain - caffeineRemain/2*(timeGap/basicTime)//시간이 흘러 줄어든 후 남은 카페인 양
-                        caffeineRemain = caffeineRemain + caffeines[timeI + 1]
-                        lineChartData.add(Entry(times[timeI + 1], caffeineRemain))
+                        val timeGap = times[timeI + 1] - currentTime//일수도 있고
+                        currentTime = times[timeI + 1]
+                        caffeineRemain = caffeineRemain - caffeineRemain/2*(timeGap/basicTime) + caffeines[timeI + 1]//시간이 흘러 줄어든 후 남은 카페인 양
+                        lineChartData.add(Entry(currentTime, caffeineRemain))
+
                         timeI++
-                        count = 1
+                        count = 0
                     }
                 }else{//이게 끝일 때
+                    currentTime = times[timeI] + (halfTime * count).toFloat()
+                    caffeineRemain = caffeineRemain!!/(2.0).toFloat()
                     lineChartData.add(Entry(currentTime, caffeineRemain))
                 }
-
-                Log.e("times2", currentTime.toString())
                 count++
-                beforeTime = currentTime
-            }
-
-        }else{
-            Log.e("caffeineVolume", "아직 입력된 값 없음")
+            }/////여기까지만
+        } else{
             lineChartData.add(Entry(0.toFloat(), 0.toFloat()))
         }
     }
-
     private fun setWeekLine(lineChart: LineChart) {
         val lineData = LineData()
-        val set1 = LineDataSet(lineChartData, "첫번째")
+        val set1 = LineDataSet(lineChartData, "체내 남은 카페인 양")
         lineData.addDataSet(set1)
         lineChart.setData(lineData)
 
         val xAxis = lineChart.xAxis
         xAxis.setDrawLabels(true)
-        xAxis.axisMaximum = 32f
-        //xAxis.axisMinimum = -240f//현재시간
+
         //xAxis.labelCount = 60
         //제일 처음 마신 시간을 그래프의 가장 앞쪽에
-        val xAxisVals = ArrayList<String>(Arrays.asList("0시", "1시", "2시", "3시", "4시", "5시", "6시", "7시", "8시", "9시", "10시", "11시", "12시", "13시", "14시", "15시", "16시", "17시", "18시", "19시", "20시", "21시", "22시", "23시", "0시", "1시", "2시", "3시", "4시", "5시", "6시", "7시"))
+        val xAxisVals = ArrayList<String>(Arrays.asList("0시", "1시", "2시", "3시", "4시", "5시", "6시", "7시", "8시", "9시", "10시", "11시", "12시", "13시", "14시", "15시", "16시", "17시", "18시", "19시", "20시", "21시", "22시", "23시", "다음날", "1시", "2시", "3시", "4시", "5시", "6시", "7시", "8시", "9시", "10시", "11시", "12시", "13시", "14시", "15시", "16시"))
         xAxis.valueFormatter = IndexAxisValueFormatter(xAxisVals)
+        xAxis.granularity = 1f
         //xAxis.granularity = 1f
-
+        xAxis.textColor = Color.BLACK
+        xAxis.position = XAxis.XAxisPosition.BOTTOM  // x축 라벨 위치
+        xAxis.setDrawLabels(true)  // Grid-line 표시
+        xAxis.setDrawAxisLine(true)  // Axis-Line 표시
         //왼쪽 y축
         val yLAxis = lineChart.axisLeft
         yLAxis.axisMinimum = 0f  // y축 최소값(고정)
@@ -126,10 +126,8 @@ class DetailActivity : AppCompatActivity() {
         yRAxis.setDrawAxisLine(false)
         yRAxis.setDrawGridLines(false)
 
-        xAxis.textColor = Color.BLACK
-        xAxis.position = XAxis.XAxisPosition.BOTTOM  // x축 라벨 위치
-        xAxis.setDrawLabels(true)  // Grid-line 표시
-        xAxis.setDrawAxisLine(true)  // Axis-Line 표시
+        lineChart.description.isEnabled = false
+        lineChart.setVisibleXRangeMaximum(35f)
         lineChart.invalidate()
     }
 
