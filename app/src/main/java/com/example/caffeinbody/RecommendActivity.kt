51af4408d9880a29664948a91ab159d5870b5238
HSHyeon <hshyeon86@gmail.com>
2022-06-06 20:17:15 +0900

    [CREATE]추천

---
+++ app/src/main/java/com/example/caffeinbody/RecommendActivity.kt
@@ -0,0 +1,37 @@
+package com.example.caffeinbody
+
+import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
+import com.example.caffeinbody.databinding.ActivityRecommendBinding
+
+class RecommendActivity : AppCompatActivity() {
+    private val binding: ActivityRecommendBinding by lazy{
+        ActivityRecommendBinding.inflate(
+            layoutInflater
+        )
+    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+        setSupportActionBar(binding.toolbar)
+        //    supportActionBar!!.setDisplayShowTitleEnabled(false)
+        supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+        supportActionBar!!.setDisplayShowTitleEnabled(false)
+
+
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem) = when (item.itemId) {
+        android.R.id.home -> {
+            finish()
+            true
+        }
+
+        else -> {
+            // If we got here, the user's action was not recognized.
+            // Invoke the superclass to handle it.
+            super.onOptionsItemSelected(item)
+        }
+    }
+}
