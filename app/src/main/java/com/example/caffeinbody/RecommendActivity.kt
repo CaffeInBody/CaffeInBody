60cda5df0abc72b8daa34c0955cff74154c9fd23
HSHyeon <hshyeon86@gmail.com>
2022-09-09 15:54:56 +0900

    [UPDATE] pullrefreshlayout

--- app/src/main/java/com/example/caffeinbody/RecommendActivity.kt
+++ app/src/main/java/com/example/caffeinbody/RecommendActivity.kt
@@ -1,11 +1,14 @@
 package com.example.caffeinbody
 
+import android.graphics.Color
 import android.os.Bundle
 import android.util.Log
 import android.view.MenuItem
 import android.view.View.GONE
 import android.view.View.VISIBLE
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.size
+import com.baoyz.widget.PullRefreshLayout
 import com.example.caffeinbody.database.DrinksDatabase
 import com.example.caffeinbody.databinding.ActivityRecommendBinding
 import kotlinx.coroutines.CoroutineScope
@@ -17,6 +20,7 @@ class RecommendActivity : AppCompatActivity() {
     private lateinit var db: DrinksDatabase
     lateinit var caffeineadapter: CaffeineAdapter
     lateinit var caffeineadapternon: CaffeineAdapter
+    var caffeine = App.prefs.currentcaffeine?.toDouble()
 
     private val binding: ActivityRecommendBinding by lazy{
         ActivityRecommendBinding.inflate(
@@ -28,17 +32,34 @@ class RecommendActivity : AppCompatActivity() {
         setContentView(binding.root)
         setSupportActionBar(binding.toolbar)
         //    supportActionBar!!.setDisplayShowTitleEnabled(false)
-        supportActionBar!!.setDisplayHomeAsUpEnabled(true)
-        supportActionBar!!.setDisplayShowTitleEnabled(false)
 
-        var caffeine = App.prefs.currentcaffeine?.toDouble()
         caffeineadapter = CaffeineAdapter(this, CaffeinCase.SMALL)
         caffeineadapternon = CaffeineAdapter(this, CaffeinCase.SMALL)
 
+        supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+        supportActionBar!!.setDisplayShowTitleEnabled(false)
+        binding.swipe.setRefreshStyle(PullRefreshLayout.STYLE_WATER_DROP)
+        binding.swipe.size
+        binding.swipe.setColorSchemeColors(Color.parseColor("#9D5812"),Color.parseColor("#9D5812"),Color.parseColor("#F8786F"),Color.parseColor("#9D5812"))
+
+
+        updateUI()
         //    caffeineadapter.type = CaffeinCase.SMALL
 
         //    val r = Runnable {
         //      try {
+
+        //    val servingsize = App.prefs.currentcaffeine
+
+        binding.swipe.setOnRefreshListener {
+            updateUI()
+            binding.swipe.setRefreshing(false)
+        }
+    }
+
+    fun updateUI(){
+
+
         CoroutineScope(Dispatchers.IO).launch {
 
             db = DrinksDatabase.getInstance(applicationContext)!!
@@ -46,49 +67,49 @@ class RecommendActivity : AppCompatActivity() {
             var nondatas = db.drinksDao().recommendnoncaffeine()
             var datas = caffeine?.let { db.drinksDao().recommendcaffeine(it) }
             Log.e("data",db.drinksDao().recommendnoncaffeine().toString())
-
+            caffeineadapter.datas.clear()
+            caffeineadapternon.datas.clear()
             if (datas?.size != 0 && datas != null) {
-                datas?.let {
+                datas.let {
                     //데이터가 많아서 랜덤으로 6개만 뽑아서 보여줌
-                    it.shuffled() //데이터 섞기
+                    var uilist = it.shuffled() //데이터 섞기
                     for (i in 0 until 6) { //6개만 뽑아서 넣음
-                        if (it.size <= i) break
-                        caffeineadapter.datas.add(it[i])
-                    }
+                        if (uilist.size <= i) break
+                        caffeineadapter.datas.add(uilist[i])
+                    }}
                     //caffeineadapter.datas.addAll(it)
-                    nondatas?.let {
-                        it.shuffled() //데이터 섞기
+                    nondatas.let {
+                        var reclist = it.shuffled() //데이터 섞기
                         for (i in 0 until 3) { //6개만 뽑아서 넣음
-                            if (it.size <= i) break
-                            caffeineadapternon.datas.add(it[i])
+                            if (reclist.size <= i) break
+
+                            caffeineadapternon.datas.add(reclist[i])
+
                         }
                         //caffeineadapter.datas.addAll(it)
 
-                    }
+                    }}
                     runOnUiThread {
+                        caffeineadapter.notifyDataSetChanged()
+                        caffeineadapternon.notifyDataSetChanged()
                         binding.nonetext.visibility = GONE //데이터 없을때 나오는 텍스트
                         binding.recyclerView.visibility = VISIBLE
                         // call the invalidate()
                     }
                 }
 
-            }
-        }
 
 
 
-            binding.recyclerView.adapter = caffeineadapter
-            caffeineadapter.notifyDataSetChanged()
 
-            binding.recyclerView2.adapter = caffeineadapternon
-            caffeineadapternon.notifyDataSetChanged()
 
-            val servingsize = App.prefs.currentcaffeine
+        binding.recyclerView.adapter = caffeineadapter
 
+
+        binding.recyclerView2.adapter = caffeineadapternon
+        caffeineadapternon.notifyDataSetChanged()
 
     }
-
-
     override fun onOptionsItemSelected(item: MenuItem) = when (item.itemId) {
         android.R.id.home -> {
             finish()
